c4eb108a8af479b3e4c780787d6bb44ae6e18eeb
rpssk <shibibhose@gmail.com>
2016-10-24 20:46:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/s1', function (req, res) {
+  res.send('hai')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
